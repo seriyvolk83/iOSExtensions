639b2f42ad92537dbccfbf4ec56449bbf07d08a0
Volkov Alexander <volk@frgroup.ru>
2016-11-16 06:04:53 +0300

    Scripts

---
+++ Bash/init.sh
@@ -0,0 +1,12 @@
+# Initialize git repository
+
+git init
+git add --all
+
+cd external
+
+# Initialize submodules with external frameworks
+
+git submodule add https://github.com/SwiftyJSON/SwiftyJSON.git
+cd ..
+
